ce1c6b38f082fb397af87b3eb6f7b6142a0b6612
Jean Dresbach <jeanr.dresbach@gmail.com>
2023-08-31 17:18:28 -0300

    login e users list finalized

--- src/users.js
+++ src/users.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import bcrypt from "bcrypt";
+import bcrypt, { compare } from "bcrypt";
 import { v4 as uuidv4 } from 'uuid';
 
 const userRouter = Router();
 const users = [];
 
 export default userRouter;
+
+userRouter.get("/", (request, response) => {
+    return response.json({
+        users
+    })
+})
 
 userRouter.post("/signup", async (request, response) => {
     const {email, name, password} = request.body;
@@ -33,3 +39,30 @@ userRouter.post("/signup", async (request, response) => {
         });
     }
 });
+
+userRouter.post("/login", async (request, response) => {
+    const {email, password} = request.body;
+    const user = users.find(user => user.email === email);
+    
+    if(!email || !password) {
+        return response.status(400).json({
+            message: "Please, fill in all fields."
+        });
+    } else if(!user) {
+        return response.status(400).json({
+            message: "Invalid email or passaword ."
+        });
+    } else {
+        const thePasswordsMatch = await bcrypt.compare(password, user.password);
+        if(!thePasswordsMatch) {
+            return response.status(400).json({
+                message: "Invalid email or passaword."
+            });
+        } else {
+            return response.status(200).json({
+                message: "User logged successfully!",
+                user
+            });
+        }
+    } 
+})
